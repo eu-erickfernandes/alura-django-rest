3e3a5bbb6bd8d434a4655ec98fc20eeee25896e2
Erick Fernandes <erickfernandess2@gmail.com>
2023-05-15 17:21:08 -0500

    Criando o README

---
+++ escola/README.md
@@ -0,0 +1,3 @@
+# Django REST Framework - Alura
+
+Neste repositório estarei publicando a aplicação criada durante a realização do curso de Django Rest da Alura.
--- escola/serializer.py
+++ escola/serializer.py
@@ -5,3 +5,8 @@ class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
